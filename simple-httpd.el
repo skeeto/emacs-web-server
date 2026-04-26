1c822619459761c902e153b4f149a393a25af05f
Daniel Mendler <mail@daniel-mendler.de>
2026-04-26 14:25:39 +0200

    Update package header
    * Add new maintainers
    * Update Package-Requires
    * Add Keywords

--- simple-httpd.el
+++ simple-httpd.el
@@ -3,9 +3,12 @@
 ;; This is free and unencumbered software released into the public domain.
 
 ;; Author: Christopher Wellons <wellons@nullprogram.com>
+;; Maintainers: Philip Kaludercic <philipk@posteo.net>,
+;;              Daniel Mendler <mail@daniel-mendler.de>
 ;; URL: https://github.com/skeeto/emacs-http-server
 ;; Version: 1.5.1
-;; Package-Requires: ((cl-lib "0.3"))
+;; Package-Requires: ((emacs "27.1"))
+;; Keywords: network
 
 ;;; Commentary:
 
